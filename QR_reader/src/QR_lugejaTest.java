9468b3227997d22a27b84214a6922ce1378dbba7
Siim Sepp <siim.sepp@gmail.com>
2020-02-06 13:10:39 +0200

    QR-lugeja

--- QR_reader/src/QR_lugejaTest.java
+++ QR_reader/src/QR_lugejaTest.java
@@ -1,9 +1,12 @@
 public class QR_lugejaTest {
     public static void main(String[] args) {
         
-        QR_lugeja lugeja = new QR_lugeja("/usr/bin/python3.6", "converter.py", "ticket.pdf");
-        
+        String pythonInterpreterLocation = "/usr/bin/python3.6";
+        String pythonFileName = "converter.py";
         String piletinimi = "ticket.pdf";
+        
+        QR_lugeja lugeja = new QR_lugeja(pythonInterpreterLocation, pythonFileName, piletinimi);
+        
         String kood = lugeja.loe_QR();
         System.out.println(kood);
     }
